14dbcde2764deb30df1216f28843507d43f49ca8
Michas <michaelcalbrecht0509@gmail.com>
2025-03-20 12:05:59 -0300

    progress bar + fixed some css
    80% done

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const weeksInfo = document.getElementById('weeks-info')
 const weeksTable = document.getElementById('weeks-table')
 const weeksLived = document.getElementById('weeks-lived')
+const lifePercentage = document.getElementById('life-percentage')
+const lifeBarProgress = document.getElementById('life-bar-progress')
 const totalWeeks = 3810 // reality check
 
 function calcWeeks(currentTime, birthdateTime) { 
     const ageSeconds = currentTime - birthdateTime
     const ageDays = Math.floor(ageSeconds / 86400000)
     const ageWeeks = Math.floor(ageDays / 7)
+    const agePercentage = Math.round(ageWeeks / totalWeeks * 100)
     
     weeksLived.textContent = ageWeeks
-    weeksInfo.style.display = 'block'
+    weeksInfo.style.display = 'flex'
+    lifePercentage.textContent = `${agePercentage}%`
+    lifeBarProgress.style.width = `${agePercentage}%`
     
     let i = 0
     weeksTable.innerHTML = ''
